3b21e2b088bc7644635ab160f5e6100754f2e791
Namita Arya <namitaarya2002@gmail.com>
2022-01-28 03:10:13 +0530

    added time stamps

--- index.js
+++ index.js
@@ -162,9 +162,11 @@ function moveLeft() {
 
   }
 }
-
   
   window.onload = init;
   console.log("initialised values");
 
+  var siteWidth = 1280;
+var scale = screen.width /siteWidth;
 
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
